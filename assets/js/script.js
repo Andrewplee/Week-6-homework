5be2523768595ee8f6731b5433eef9e48d02ef33
Andrew Lee <alee68@fordham.edu>
2022-05-23 23:15:20 -0400

    added comments

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// by entering the city name, the city data and the forecast will appaer
+// to locate the city, we need longitude and latitute
+// with the longitute and latitute, we can pinpoint the city and the name will appear along with the date (maybe use moment to show current date)
+// data that we will need to show is are the temerature, wind, humidity and UV index:
+
+//as for the forecast, we will use the long and lat to pin point the city and create the forecast based on today's date
+// same thing for the temp, wind, and humidity
+
+// upon typing the city name, we will need to save the city name up to the local storage and call it down as a button
+// set it so that upon clicking the button, the value or text on the button will trigger the search function and pinpoint again
