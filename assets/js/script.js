805fd0e03884900cd55045d18963a72483953eb7
Andrew Lee <alee68@fordham.edu>
2022-05-28 14:09:59 -0400

    render weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var locationEl = document.getElementById("location");
 var cityArray = [];
 
 var getCityName = function () {
-	JSON.parse(localStorage.getItem("cityList")) || [];
+	cityArray = JSON.parse(localStorage.getItem("cityList")) || [];
 };
 var setCityName = function () {
 	localStorage.setItem("cityList", JSON.stringify(cityArray));
@@ -15,73 +15,158 @@ var setCityName = function () {
 // in local storage, call the city back into buttons
 //include rendering the buttons
 var limit;
+
 var renderCityHistory = function () {
+	cityHistoryEl.textContent = "";
+
 	if (cityArray.length < 10) {
 		limit = cityArray.length;
 	} else {
 		limit = 10;
 	}
 
-	for (i = 0; i < limit; i++) {
-		var btnEl = document.createElement("button");
-		btnEl.textContent = cityArray[i];
-		btnEl.setAttribute("class", "cityHistoryBtn");
-		cityHistoryEl.appendChild(btnEl);
+	// for (i = 0; i < limit; i++) {
+	// 	vyar btnEl = document.createElement("button");
+	// 	btnEl.textContent = cityArra[i];
+	// 	btnEl.setAttribute("class", "cityHistoryBtn");
+	// 	cityHistoryEl.appendChild(btnEl);
+	// }
+
+	// have to target the key (cityList) to get the value in a loop
+	if (cityArray.length <= limit) {
+		cityArray.forEach(function (city) {
+			var btnEl = document.createElement("button");
+			btnEl.textContent = city;
+			btnEl.setAttribute("class", "cityHistoryBtn");
+			cityHistoryEl.appendChild(btnEl);
+
+			btnEl.addEventListener("click", function (event) {
+				event.preventDefault();
+				getCityName(getCoordinates(this.textContent));
+			});
+		});
 	}
-
 	// if history button is pressed, then get the city name and run the getCoordinates function
-	btnEl.addEventListener("click", function (event) {
-		event.preventDefault();
-		getCoordinates(cityArray[i]);
-	});
-
-	// forEach(function (item) {
 	// 	var btnEl = document.createElement("button");
 	// 	btnEl.textContent = `${item}`;
 	// 	btnEl.setAttribute("class", "child");
 	// 	cityHistoryEl.appendChild(btnEl);
-	// });
 };
 
 // by entering the city name, the city data and the forecast will appear
 searchBtn.addEventListener("click", function (event) {
 	event.preventDefault();
 
-	if (!this.value) {
+	var cityValue = cityEl.value;
+
+	if (!cityValue) {
 		return;
 	}
-	if (!cityArray.includes(this.value)) {
-		this.value.push(cityName);
+	if (!cityArray.includes(cityValue)) {
+		cityArray.push(cityValue);
 		setCityName();
 	}
+	console.log(cityValue);
+
 	getCityName();
 	renderCityHistory();
-	getCoordinates();
+	getCoordinates(cityValue);
 });
 
 // to locate the city, we need longitude and latitute
-var getCoordinates = function (cityCoordinates) {
+var getCoordinates = function (cityName) {
+	locationEl.textContent = "";
 	var coordinates =
 		"http://api.openweathermap.org/geo/1.0/direct?q=" +
-		cityCoordinates +
-		"&limit={limit}&appid=72089c6cb9be7989ea34e3e3aad0d5ed";
+		cityName +
+		"&appid=72089c6cb9be7989ea34e3e3aad0d5ed";
 
 	fetch(coordinates).then(function (response) {
 		if (response.ok) {
 			response.json().then(function (data) {
 				var cityNameEl = document.createElement("h3");
-				cityNameEl.textContent = data.name;
+				cityNameEl.textContent = data[0].name;
 				locationEl.appendChild(cityNameEl);
-
-				getCityWeather(data.lat, data.lon);
+				console.log(data);
+				getCityWeather(data[0].lat, data[0].lon);
 			});
-		} else {
-			location.reload();
 		}
+		//  else {
+		// 	location.reload();
+		// }
 	});
 };
 
 // order matters when rendering history values
+// data that we will need to show is are the temerature, wind, humidity and UV index:
+var showCityWeather = function (placeholder) {
+	var imgEl = document.createElement("img");
+	imgEl.setAttribute(
+		"src",
+		`http://openweathermap.org/img/wn/${placeholder.current.weather[0].icon}@2x.png`
+	);
+
+	locationEl.appendChild(imgEl);
+
+	var temperatureEl = document.createElement("div");
+	temperatureEl.textContent = "Temp: " + placeholder.current.temp + " F";
+	locationEl.appendChild(temperatureEl);
+
+	var windSpeedEl = document.createElement("div");
+	windSpeedEl.textContent = "Wind: " + placeholder.current.wind_speed;
+	locationEl.appendChild(windSpeedEl);
+
+	var humidityEl = document.createElement("div");
+	humidityEl.textContent = "Humidity: " + placeholder.current.humidity;
+	locationEl.appendChild(humidityEl);
+
+	var uviEl = document.createElement("div");
+	uviEl.textContent = "UV Index: " + placeholder.current.uvi;
+	locationEl.appendChild(uviEl);
+};
+
+//as for the forecast, we will use the lon and lat to pinpoint the city and create the forecast based on today's date
+// same thing for the temp, wind, and humidity
+var showForecastWeather = function (placeholder) {
+	var temperatureEl = document.createElement("div");
+	temperatureEl.textContent = "Temp: " + placeholder.current.temp + " F";
+	locationEl.appendChild(temperatureEl);
+
+	var windSpeedEl = document.createElement("div");
+	windSpeedEl.textContent = "Wind: " + placeholder.current.wind_speed;
+	locationEl.appendChild(windSpeedEl);
+
+	var humidityEl = document.createElement("div");
+	humidityEl.textContent = "Humidity: " + placeholder.current.humidity;
+	locationEl.appendChild(humidityEl);
+
+	var uviEl = document.createElement("div");
+	uviEl.textContent = "UV Index: " + placeholder.current.uvi;
+	locationEl.appendChild(uviEl);
+};
+
+var forecastWeather = function () {
+	for (i = 0; i < 40; i += 8) {
+		// create each of the containers and assign bootstrap
+		// attend all information as child to the container
+
+		var temperatureEl = document.createElement("div");
+		temperatureEl.textContent = "Temp: " + placeholder.current.temp + " F";
+		locationEl.appendChild(temperatureEl);
+
+		var windSpeedEl = document.createElement("div");
+		windSpeedEl.textContent = "Wind: " + placeholder.current.wind_speed;
+		locationEl.appendChild(windSpeedEl);
+
+		var humidityEl = document.createElement("div");
+		humidityEl.textContent = "Humidity: " + placeholder.current.humidity;
+		locationEl.appendChild(humidityEl);
+
+		var uviEl = document.createElement("div");
+		uviEl.textContent = "UV Index: " + placeholder.current.uvi;
+		locationEl.appendChild(uviEl);
+	}
+};
 
 // with the longitute and latitute, we can pinpoint the city and the name will appear along with the date (maybe use moment to show current date)
 var getCityWeather = function (cityLat, cityLon) {
@@ -90,37 +175,27 @@ var getCityWeather = function (cityLat, cityLon) {
 		cityLat +
 		"&lon=" +
 		cityLon +
-		"&appid=72089c6cb9be7989ea34e3e3aad0d5ed";
+		"&appid=72089c6cb9be7989ea34e3e3aad0d5ed&units=imperial";
+
+	var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=72089c6cb9be7989ea34e3e3aad0d5ed`;
 
 	fetch(apiUrl).then(function (response1) {
 		if (response1.ok) {
 			response1.json().then(function (weatherDetails) {
-				showCityweather(weatherDetails);
+				showCityWeather(weatherDetails);
+			});
+		}
+	});
+
+	fetch(forecastUrl).then(function (response2) {
+		if (response2.ok) {
+			response2.json().then(function (weatherDetails) {
+				forecastWeather(weatherDetails);
 			});
 		}
 	});
 };
 
-// data that we will need to show is are the temerature, wind, humidity and UV index:
-var showCityWeather = function (placeholder) {
-	var temperatureEl = locationEl.createElement("span");
-	temperatureEl.textContent = "Temp: " + placeholder.current.temp;
-	locationEl.appendChild(temperatureEl);
-
-	var windSpeedEl = locationEl.createElement("span");
-	windSpeedEl.textContent = "Wind: " + placeholder.current.wind_speed;
-	locationEl.appendChild(windSpeedEl);
-
-	var humidityEl = locationEl.createElement("span");
-	humidityEl.textContent = "Humidity: " + placeholder.current.temp;
-	locationEl.appendChild(humidityEl);
-
-	var uviEl = locationEl.createElement("span");
-	uviEl.textContent = "UV Index: " + placeholder.current.temp;
-	locationEl.appendChild(uviEl);
-};
-
-//as for the forecast, we will use the lon and lat to pin point the city and create the forecast based on today's date
-// same thing for the temp, wind, and humidity
-
 //5 day will basically follow same format as city weather, except we have to create 5 different containers, basically 1 extra step in creating the container for 5 day
+getCityName();
+renderCityHistory();
